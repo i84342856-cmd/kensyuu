d5cd8e3d6a614af18c87ee34551fbd44b1a7d902
mot-p <mot-p@moatpc02>
2026-02-04 18:35:24 +0900

    コード1-3　P.53

--- trainingjava/src/com/example/cp1/Code2.java
+++ trainingjava/src/com/example/cp1/Code2.java
@@ -2,8 +2,9 @@ package com.example.cp1;
 
 public class Code2 {
 	public static void main(String[] args) {
-		int age;
-		age = 30;
-		System.out.println(age);
+		int age = 30;
+		System.out.println("私の年齢は" + age);
+		age = 31;
+		System.out.println("私の年齢は" + age);
 	}
 }
