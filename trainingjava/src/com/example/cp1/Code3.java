976bf4c5d15f3b2dccd01c201acdbbd6fcc05fdd
mot-p <mot-p@moatpc02>
2026-02-05 21:10:51 +0900

    code3

--- trainingjava/src/com/example/cp1/Code3.java
+++ trainingjava/src/com/example/cp1/Code3.java
@@ -1,11 +1,12 @@
 package com.example.cp1;
 
-public class code3 {
+public class Code3 {
 	public static void main(String[] args) {
 		boolean result = true;
 		char favoriteCharacter = '駆';
 		double pi = 3.14;
 		long number = 31415L;
 		String msg = "ポイントのダメージを与えた";
+		System.out.println(msg + " : " + result + " / " + favoriteCharacter + " / " + pi + " / " + number);
 	}
 }
