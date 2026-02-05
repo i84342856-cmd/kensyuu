e25309780c6bee6e3fe7d393c20f1f172c76fb55
mot-p <mot-p@moatpc02>
2026-02-05 10:44:20 +0900

    練習2-1　P.95

--- trainingjava/src/com/example/cp1/Code5.java
+++ trainingjava/src/com/example/cp1/Code5.java
@@ -8,10 +8,16 @@ public class Code5 {
 		int r = new java.util.Random().nextInt(90);
 		System.out.println("あなたは" + r + "になりますよね");
 		
+		// キーボード入力して回答するメソッド //
 		System.out.println("あなたの名前");
 		String name = new java.util.Scanner(System.in).nextLine();
 		System.out.println("あなたの年齢");
 		int ag = new java.util.Scanner(System.in).nextInt();
 		System.out.println("ようこそ" + ag + name);
+		
+		//優先順位を上げさせる//
+		int x = 5;
+		int y = 10;
+		System.out.println("あなたの年齢" + (x + y));
 	}
 }
