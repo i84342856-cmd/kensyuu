ffaeb896f727f7a7f3cd7c61484850376fcb3d66
mot-p <mot-p@moatpc02>
2026-02-05 10:37:28 +0900

    コード2-15　P.93

--- trainingjava/src/com/example/cp1/Code5.java
+++ trainingjava/src/com/example/cp1/Code5.java
@@ -7,5 +7,11 @@ public class Code5 {
 		System.out.println("あなたは" + n + "になりますよね");
 		int r = new java.util.Random().nextInt(90);
 		System.out.println("あなたは" + r + "になりますよね");
+		
+		System.out.println("あなたの名前");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("あなたの年齢");
+		int ag = new java.util.Scanner(System.in).nextInt();
+		System.out.println("ようこそ" + ag + name);
 	}
 }
