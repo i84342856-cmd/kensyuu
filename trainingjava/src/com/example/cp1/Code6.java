e1e8212f91f20e93a00c3c8f3bd94c56b1b5aca7
mot-p <mot-p@moatpc02>
2026-02-05 11:04:18 +0900

    練習2-3　P.98

---
+++ trainingjava/src/com/example/cp1/Code6.java
@@ -0,0 +1,17 @@
+package com.example.cp1;
+
+public class Code6 {
+	public static void main(String[] args) {
+		System.out.println("ようこそ占いの館");
+		System.out.println("名前を入力");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("年齢を入力");
+		String ageString = new java.util.Scanner(System.in).nextLine();
+		int age = Integer.parseInt(ageString);
+		int fortune = new java.util.Random().nextInt(4);
+		++fortune;
+		System.out.println("占いの結果");
+		System.out.println(age + "歳" + name + "の運気番号は" + fortune);
+		
+	}
+}
