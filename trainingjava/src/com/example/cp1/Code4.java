89635ac586584c2bac06af33a66b7a79d3156dcf
mot-p <mot-p@moatpc02>
2026-02-05 09:34:54 +0900

    コード2-1　P.62

---
+++ trainingjava/src/com/example/cp1/Code4.java
@@ -0,0 +1,12 @@
+package com.example.cp1;
+
+public class Code4 {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 20;
+		b = a + b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
