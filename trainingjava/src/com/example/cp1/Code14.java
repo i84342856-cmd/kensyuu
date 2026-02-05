4a08ed19d2cece91bc87f64231bf6cb3138c5a9b
mot-p <mot-p@moatpc02>
2026-02-05 17:12:25 +0900

    メソッド④

---
+++ trainingjava/src/com/example/cp1/Code14.java
@@ -0,0 +1,33 @@
+package com.example.cp1;
+
+public class Code14 {
+	public static void incArray(int[] array) {
+		for(int i = 0; i< array.length;i++) {
+			array[i]++;
+		}
+	}
+	
+	public static int[] makeArray(int size) {
+		int[] newArray = new int[size];
+		for(int i = 0 ; i < newArray.length; i++) {
+			newArray[i] = i;
+		}
+		return newArray;
+	}
+	
+	// 呼び出し元の配列のアドレスが呼び出し先の引数にコピーされる //
+	public static void main(String[] args) {
+		int[] array = {2,3,4};
+		incArray(array);
+		for(int i : array) {
+			System.out.println(i);
+		}
+		
+		int[] array2 = makeArray(3);
+		for(int i : array2 ) {
+			System.out.println(i);
+		}
+		
+	}
+	
+}
