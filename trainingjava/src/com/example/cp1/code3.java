68fccb6a92bac1cb6a4516cbe75c5bcc7562e657
mot-p <mot-p@moatpc02>
2026-02-05 09:29:49 +0900

    P.59 1-3

---
+++ trainingjava/src/com/example/cp1/code3.java
@@ -0,0 +1,11 @@
+package com.example.cp1;
+
+public class code3 {
+	public static void main(String[] args) {
+		boolean result = true;
+		char favoriteCharacter = '駆';
+		double pi = 3.14;
+		long number = 31415L;
+		String msg = "ポイントのダメージを与えた";
+	}
+}
