62d89a810abe382d222b2525030f6c653397a560
mot-p <mot-p@moatpc02>
2026-02-05 10:28:36 +0900

    コード2-13　P.90

---
+++ trainingjava/src/com/example/cp1/Code5.java
@@ -0,0 +1,9 @@
+package com.example.cp1;
+
+public class Code5 {
+	public static void main(String[] args) {
+		String age = "31";
+		int n = Integer.parseInt(age);
+		System.out.println("あなたは" + n + "になりますよね");
+	}
+}
