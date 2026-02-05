05f4bf88142f4b43002a043b09ab0e4a939feaa1
mot-p <mot-p@moatpc02>
2026-02-05 16:46:08 +0900

    メソッド３

---
+++ trainingjava/src/com/example/cp1/Code13.java
@@ -0,0 +1,29 @@
+package com.example.cp1;
+
+public class Code13 {
+	public static int add(int x, int y) {
+		int ans = x + y;
+		return ans;
+		// return　後に書いてもエラーになる//
+	}
+	
+	public static int add(int x, int y, int z) {
+		return x + y + z;
+	}
+	
+	public static void printArray(int[] array) {
+		for(int element : array) {
+			System.out.print(element);
+		}
+	}
+	
+	public static void main(String[] args) {
+		int ans = add(50,80);
+		System.out.println(ans);
+		System.out.println(add(50,80,90));
+		// メソッド名が同じでも仮引数がちがえばオーバーロードできる//
+		int[] array = {2,3,5};
+		printArray(array);
+	}
+
+}
