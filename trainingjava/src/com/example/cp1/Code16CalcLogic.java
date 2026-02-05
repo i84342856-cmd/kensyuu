3d25f45339fae091267a1ab27700b5f5df08f11a
mot-p <mot-p@moatpc02>
2026-02-05 18:04:28 +0900

    複数クラスの作成

---
+++ trainingjava/src/com/example/cp1/Code16CalcLogic.java
@@ -0,0 +1,10 @@
+package com.example.cp1;
+
+public class Code16CalcLogic {
+	public static int tasu(int a, int b) {
+		return a + b;
+	}
+	public static int hiku(int a, int b) {
+		return a - b;
+	}
+}
