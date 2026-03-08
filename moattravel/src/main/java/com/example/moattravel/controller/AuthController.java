4fa412207273cb950b62250d0110a41b9c881b29
mot-p <mot-p@moatpc02>
2026-03-08 18:57:21 +0900

    @PostMapping("/signup")   追加 3/7

--- moattravel/src/main/java/com/example/moattravel/controller/AuthController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/AuthController.java
@@ -2,13 +2,26 @@ package com.example.moattravel.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.moattravel.form.SignupForm;
+import com.example.moattravel.service.UserService;
 
 @Controller
 public class AuthController {
-
+	
+	private final UserService userService;
+	
+	public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+	
 	@GetMapping("/login")
 	public String login() {   
 		 return "auth/login";
@@ -19,4 +32,34 @@ public class AuthController {
 		model.addAttribute("signupForm", new SignupForm());
 		return "auth/signup";
 	}
+	
+	@PostMapping("/signup")
+    public String signup(@ModelAttribute @Validated SignupForm signupForm, 
+                         BindingResult bindingResult, 
+                         RedirectAttributes redirectAttributes) 
+    {
+        // 1. メールアドレスが登録済みかチェック
+        if (userService.isEmailRegistered(signupForm.getEmail())) {
+            FieldError fieldError = new FieldError(bindingResult.getObjectName(), "email", "すでに登録済みのメールアドレスです。");
+            bindingResult.addError(fieldError);
+        }
+
+        // 2. パスワードの一致チェック
+        if (!userService.isSamePassword(signupForm.getPassword(), signupForm.getPasswordConfirmation())) {
+            FieldError fieldError = new FieldError(bindingResult.getObjectName(), "password", "パスワードが一致しません。");
+            bindingResult.addError(fieldError);
+        }
+
+        // 3. バリデーションエラーがあればフォームに戻す
+        if (bindingResult.hasErrors()) {
+            return "auth/signup";
+        }
+
+        // 4. ユーザー作成と完了メッセージの設定
+        userService.create(signupForm);
+        redirectAttributes.addFlashAttribute("successMessage", "会員登録が完了しました。");
+
+        return "redirect:/";
+    }
+	
 }
