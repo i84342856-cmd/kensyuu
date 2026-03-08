e879ffed1e538d74bb358938b3d29620bdd62056
mot-p <mot-p@moatpc02>
2026-03-08 16:37:58 +0900

    @GetMapping("/signup") 追加  3/7

--- moattravel/src/main/java/com/example/moattravel/controller/AuthController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.moattravel.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.moattravel.form.SignupForm;
 
 @Controller
 public class AuthController {
@@ -9,17 +12,11 @@ public class AuthController {
 	@GetMapping("/login")
 	public String login() {   
 		 return "auth/login";
-		 
-	/* 
-	 * 1. Spring Securityが「POST」を横取りしてくれる
-	通常、私たちが作るコントローラーは「自分が書いたメソッド」が動きます。しかし、ログインに関しては Spring Securityというフレームワークが、背後で待ち構えています。
-	HTML <form th:action="@{/login}" method="post">
-	そして、設定クラス（WebSecurityConfig）でこう設定しました：
-	.formLogin((form) -> form
-    .loginProcessingUrl("/login") // ←ここがポイント！
-    ...
-	)
-	この設定があるおかげで、ユーザーがログインボタンを押して /login にPOSTリクエストが飛んだ瞬間、
-	Spring Securityが「あ、これは俺の仕事だ！」と横取りして、自動的にユーザー名やパスワードのチェック（認証処理）を行ってくれます。*/
+	}
+	
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("signupForm", new SignupForm());
+		return "auth/signup";
 	}
 }
