1478fc3ce221e74af31e831b0ad325f036070638
mot-p <mot-p@moatpc02>
2026-03-08 17:58:11 +0900

    findByNameメソッド追加 3/7

--- moattravel/src/main/java/com/example/moattravel/repository/RoleRepository.java
+++ moattravel/src/main/java/com/example/moattravel/repository/RoleRepository.java
@@ -7,4 +7,5 @@ import com.example.moattravel.entity.Role;
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     // 基本的なCRUD操作（保存、検索、削除など）はJpaRepositoryが提供するため、
     // 特殊な検索が必要なければ、中身は空のままで動作します。
+	public Role findByName(String name);
 }
