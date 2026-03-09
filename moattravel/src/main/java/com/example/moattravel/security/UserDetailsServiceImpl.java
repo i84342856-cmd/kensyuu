79ace64532a4287d347f72d4c00c462e987134a9
mot-p <mot-p@moatpc02>
2026-03-09 17:50:12 +0900

    インデント

--- moattravel/src/main/java/com/example/moattravel/security/UserDetailsServiceImpl.java
+++ moattravel/src/main/java/com/example/moattravel/security/UserDetailsServiceImpl.java
@@ -23,42 +23,41 @@ import com.example.moattravel.repository.UserRepository;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    // コンストラクタ注入（DIコンテナからリポジトリを受け取る）
-    public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+	// コンストラクタ注入（DIコンテナからリポジトリを受け取る）
+	public UserDetailsServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
-   
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        try {
-            // 1. DBからUserエンティティを取得（ここでデータが「セット」される源泉が見つかる）
-            User user = userRepository.findByEmail(email);
-            
-            if (user == null) {
-                throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
-            }
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		try {
+			// 1. DBからUserエンティティを取得（ここでデータが「セット」される源泉が見つかる）
+			User user = userRepository.findByEmail(email);
 
-            /* .user.getRole() → userオブジェクト中に入っている Roleクラスオブジェクトを取り出す。
-             * .getName() → そのRoleオブジェクトの name（文字列） を取り出す。
-             * 「役割（Role）」には名前以外にも情報を持たせることが多いため、独立したクラス（型）にすることが一般的です。*/
-            String userRoleName = user.getRole().getName();
-            
-            Collection<GrantedAuthority> authorities = new ArrayList<>();
-            
-            /* GrantedAuthority はただの「インターフェース（規格）」
-             * インターフェースは new できないので、Spring Securityが用意する「実体」である SimpleGrantedAuthorityを使います。*/
-            authorities.add(new SimpleGrantedAuthority(userRoleName));
+			if (user == null) {
+				throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
+			}
 
-            // 3. 【重要】ここで UserDetailsImpl を new して、DBから取ってきた user と authorities を渡す
-            // これにより、Securityが管理する「ログイン証」が完成する
-            return new UserDetailsImpl(user, authorities);
+			/* .user.getRole() → userオブジェクト中に入っている Roleクラスオブジェクトを取り出す。
+			 * .getName() → そのRoleオブジェクトの name（文字列） を取り出す。
+			 * 「役割（Role）」には名前以外にも情報を持たせることが多いため、独立したクラス（型）にすることが一般的です。*/
+			String userRoleName = user.getRole().getName();
 
-        } catch (Exception e) {
-            // 例外が発生した場合は一律で「見つからない」としてSecurityに通知する
-            throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
-        }
-    }
+			Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+			/* GrantedAuthority はただの「インターフェース（規格）」
+			 * インターフェースは new できないので、Spring Securityが用意する「実体」である SimpleGrantedAuthorityを使います。*/
+			authorities.add(new SimpleGrantedAuthority(userRoleName));
+
+			// 3. 【重要】ここで UserDetailsImpl を new して、DBから取ってきた user と authorities を渡す
+			// これにより、Securityが管理する「ログイン証」が完成する
+			return new UserDetailsImpl(user, authorities);
+
+		} catch (Exception e) {
+			// 例外が発生した場合は一律で「見つからない」としてSecurityに通知する
+			throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
+		}
+	}
 }
