059c807f8af36ddf4fc95e80984110d4824bdac4
mot-p <mot-p@moatpc02>
2026-03-10 16:30:54 +0900

    新着の民宿を最大10件取得

--- moattravel/src/main/java/com/example/moattravel/controller/HomeController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/HomeController.java
@@ -1,12 +1,36 @@
 package com.example.moattravel.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.moattravel.entity.House;
+import com.example.moattravel.repository.HouseRepository;
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
-	public String index	() {
-		return 	"index";
-	}
+	private final HouseRepository houseRepository;
+	
+	// コンストラクタ注入
+    public HomeController(HouseRepository houseRepository) {
+        this.houseRepository = houseRepository;
+    }
+    
+    /**
+     * トップページを表示します。
+     * 新着の民宿を最大10件取得してモデルに登録します。
+     */
+    
+    @GetMapping("/")
+    public String index(Model model) {
+        // リポジトリから新着10件を取得
+        List<House> newHouses = houseRepository.findTop10ByOrderByCreatedAtDesc();
+        
+        // ビュー（HTML）へ渡すデータをセット
+        model.addAttribute("newHouses", newHouses);
+
+        return "index";
+    }
 }
