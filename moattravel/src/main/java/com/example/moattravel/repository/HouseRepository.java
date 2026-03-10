8a35e82d04031cc95b7abaf088bf795e19bd7eb9
mot-p <mot-p@moatpc02>
2026-03-10 11:57:19 +0900

    findByNameLikeOrAddressLike   findByPriceLessThanEqual   追加

--- moattravel/src/main/java/com/example/moattravel/repository/HouseRepository.java
+++ moattravel/src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -8,6 +8,16 @@ import com.example.moattravel.entity.House;
 
 public interface HouseRepository  extends JpaRepository<House, Integer> {
 	public Page<House> findByNameLike(String keyword, Pageable pageable);
+	
+	// 民宿名または住所で検索（部分一致）
+    Page<House> findByNameLikeOrAddressLike(String nameKeyword, String addressKeyword, Pageable pageable);
+
+    //住所（エリア）で検索（部分一致）
+    Page<House> findByAddressLike(String area, Pageable pageable);
+
+    // 指定された金額以下の宿泊料金で検索
+    Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);
+    
 	}
 
 
