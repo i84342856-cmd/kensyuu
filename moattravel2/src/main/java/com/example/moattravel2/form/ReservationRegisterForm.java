1c5732802217318b0752a595715b6ab44730cde5
mot-p <mot-p@moatpc02>
2026-03-12 17:54:38 +0900

    土台

--- moattravel2/src/main/java/com/example/moattravel2/form/ReservationRegisterForm.java
+++ moattravel2/src/main/java/com/example/moattravel2/form/ReservationRegisterForm.java
@@ -1,20 +1,5 @@
 package com.example.moattravel2.form;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class ReservationRegisterForm {    
-    private Integer houseId;
-        
-    private Integer userId;    
-        
-    private String checkinDate;    
-        
-    private String checkoutDate;    
-    
-    private Integer numberOfPeople;
-    
-    private Integer amount;    
+   
 }
