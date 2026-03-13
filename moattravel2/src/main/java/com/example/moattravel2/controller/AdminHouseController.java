ffc2ce7edd771c8a9da265554663829a96251e18
mot-p <mot-p@moatpc02>
2026-03-13 16:41:47 +0900

    第7章～10章

--- moattravel2/src/main/java/com/example/moattravel2/controller/AdminHouseController.java
+++ moattravel2/src/main/java/com/example/moattravel2/controller/AdminHouseController.java
@@ -1,6 +1,48 @@
 package com.example.moattravel2.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.moattravel2.entity.House;
+import com.example.moattravel2.repository.HouseRepository;
+
+@Controller
+@RequestMapping("/admin/houses")
 public class AdminHouseController {
-    
+	private final HouseRepository houseRepository;
+
+	public AdminHouseController(HouseRepository houseRepository) {
+		this.houseRepository = houseRepository;
+	}
+
+	@GetMapping
+	public String index(Model model,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+			@RequestParam(name = "keyword", required = false) String keyword) {
+		Page<House> housePage = houseRepository.findAll(pageable);
+
+		if (keyword != null && keyword.isEmpty())
+			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
+
+		model.addAttribute("housePage", housePage);
+		model.addAttribute("keyword", keyword);
+		return "admin/houses/index";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable(name="id")Integer id,Model model) {
+		House house = houseRepository.findById(id).orElseThrow(() -> new RuntimeException("指定された民宿が見つかりません。"));
+		// 標準ではないので、使用しない。　House house = houseRepository.getReferenceById(id);
+			model.addAttribute("house",house);
+			return "admin/houses/show";
+	}
+
 }
--- moattravel2/src/main/java/com/example/moattravel2/repository/HouseRepository.java
+++ moattravel2/src/main/java/com/example/moattravel2/repository/HouseRepository.java
@@ -1,5 +1,7 @@
 package com.example.moattravel2.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.moattravel2.entity.House;
 
 @Repository
 public interface HouseRepository extends JpaRepository<House,Integer> {	
-    
+    public Page<House> findByNameLike(String keyword,Pageable pageable);
 }
